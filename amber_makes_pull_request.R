09babc509248cdb84aaac7742ce6d83ce5d74e21
Runyon <arunyon@nps.gov>
2020-03-23 08:34:50 -0600

    Adding to original script

--- amber_makes_pull_request.R
+++ amber_makes_pull_request.R
@@ -2,4 +2,7 @@
 # Please fork this repo and make a change to this script
 # Submit a pull request to me
 
-# Amber makes a change
+# Changes from 0323
+x<-rnorm(4,2,.5)
+y<-seq(.5,2,.5)
+plot(x,y,type="l")
